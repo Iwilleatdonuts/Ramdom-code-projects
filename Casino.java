51b27c5f5acb405eedd6525174ccf2b564368f05
Iwilleatdonuts <tjtang0310@gmail.com>
2025-01-21 00:49:49 -0500

    added card and suit generator for casino

--- Casino.java
+++ Casino.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class Casino {
 
-    private static String name;
-    private static int age;
-    private static int currency;
-    private static casinoGames state;
+    static String[] cards = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+            "Queen", "King", "Joker" };
+    static String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+    static String name;
+    static int age;
+    static int currency;
+    static casinoGames state;
+    static boolean casinoIsRunning;
 
     public static void main(String[] args) throws Exception {
 
@@ -13,40 +18,134 @@ public class Casino {
 
         state = casinoGames.HOME;
 
-        updatePlayerInfo(scanner, true);
+        age = 0;
 
-        switch (state) {
+        casinoIsRunning = true;
 
-            case HOME:
+        System.out.println(suits[drawSuitIndex()]);
 
-                System.out.println("You currently have $" + currency + "\nWhat would you like to do?");
-                System.out.println("\t1 - Play games");
-                System.out.println("\t2 - Update player information");
-                System.out.println("\t3 - Leave casino");
+        while (casinoIsRunning) {
 
-                int option = scanner.nextInt();
+            switch (state) {
 
-                if (option == 1) {
+                case HOME:
 
-                    state = casinoGames.GAME_SELECTOR;
+                    while (age < 19) {
 
-                } else if (option == 2) {
+                        updatePlayerInfo(scanner, true, 3000);
 
-                    System.out.println("Will you also be resetting your currency back to $3000?\nType Y/N");
-                    
+                    }
 
-                    state = casinoGames.BLACKJACK;
-                }
+                    System.out.println("\nYou currently have $" + currency + ", what would you like to do?\n");
+                    System.out.println("\t1 - Play games");
+                    System.out.println("\t2 - Update player information");
+                    System.out.println("\t3 - Leave casino");
 
-                break;
+                    int homeOptions = scanner.nextInt();
+                    scanner.nextLine();
 
-            case GAME_SELECTOR:
+                    if (homeOptions == 1) {
 
-                break;
+                        state = casinoGames.GAME_SELECTOR;
 
-            case BLACKJACK:
+                    } else if (homeOptions == 2) {
 
-                break;
+                        state = casinoGames.RESETTING;
+
+                    } else {
+
+                        System.out.println("\nYou are leaving the casino with $" + currency + ", have a great day!");
+                        casinoIsRunning = false;
+                    }
+
+                    break;
+
+                case RESETTING:
+
+                    System.out
+                            .println("\nWould you like to reset your currency back to $3000, or to a custom amount?\n");
+                    System.out.println("\t1 - Yes, reset to $3000");
+                    System.out.println("\t2 - Yes, reset to custom amount");
+                    System.out.println("\t3 - No");
+
+                    int resetOptions = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (resetOptions == 1) {
+
+                        updatePlayerInfo(scanner, true, 3000);
+
+                    } else if (resetOptions == 2) {
+
+                        System.out.println("How much would you like to recieve?");
+
+                        int newAmount = scanner.nextInt();
+                        scanner.nextLine();
+
+                        updatePlayerInfo(scanner, true, newAmount);
+
+                    } else {
+
+                        updatePlayerInfo(scanner, false, 0);
+
+                    }
+
+                    state = casinoGames.HOME;
+
+                    break;
+
+                case GAME_SELECTOR:
+
+                    System.out.println("\nWhat game would you like to play?\n");
+                    System.out.println("\t1 - Blackjack");
+                    System.out.println("\t2 - what the sigma");
+                    System.out.println("\t5 - Return to home");
+
+                    int gameOptions = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (gameOptions == 1) {
+
+                        System.out.println("\nWelcome to Blackjack!");
+                        state = casinoGames.BLACKJACK;
+
+                    } else {
+
+                        state = casinoGames.HOME;
+
+                    }
+
+                    break;
+
+                case BLACKJACK:
+
+                    System.out.println("\nWhat would you like to do?");
+                    System.out.println("\t1 - Play");
+                    System.out.println("\t2 - Leave");
+
+                    int blackJackOptions = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (blackJackOptions == 1) {
+
+                        boolean gameIsRunning = true;
+
+                        while (gameIsRunning) {
+
+                            System.out.println("Your starting ");
+
+                        }
+
+                    } else {
+
+                        System.out.println("Good job, " + name);
+                        state = casinoGames.HOME;
+
+                    }
+
+                    break;
+
+            }
 
         }
 
@@ -54,42 +153,55 @@ public class Casino {
 
     }
 
-    static boolean updatePlayerInfo(Scanner scan, boolean resetCurrency) {
+    static void updatePlayerInfo(Scanner scan, boolean resetCurrency, int resetAmount) {
 
-        System.out.println("Please enter name");
+        System.out.println("\nPlease enter name");
         name = scan.nextLine();
 
-        System.out.println("Please enter Age");
+        System.out.println("\nPlease enter Age");
         age = scan.nextInt();
+        scan.nextLine();
 
         if (resetCurrency) {
-            currency = 3000;
+            currency = resetAmount;
         }
 
         if (age < 19) {
-            System.out.println("You are below the legal age for gambling in Ontario, please leave");
-            return false;
+            System.out.println("\nYou are below the legal age for gambling in Ontario, please leave and try again \n");
         } else {
-            System.out.println("Welcome to the casino, " + name);
-            return true;
+            System.out.println("\nWelcome to the casino, " + name);
         }
 
     }
 
-    /*
-     * ill make proper comments at some point, but for now minCard should only ever
-     * be as low as 1(ace), and maxCard should only ever be as high as 14(joker)
-     */
-    static int drawCard(int minCard, int maxCard) {
+    static int drawCardIndex(boolean includeJokers) {
 
-        int range = (maxCard - minCard) + 1;
-        return (int) (Math.random() * range) + minCard;
+        int range = 13;
+
+        if (includeJokers) {
+
+            range = 14;
+
+        }
+
+        int cardIndex = (int) (Math.random() * range) + 1;
+
+        return cardIndex - 1;
+
+    }
+
+    static int drawSuitIndex() {
+
+        int suitIndex = (int) (Math.random() * 4) + 1;
+
+        return suitIndex - 1;
 
     }
 
     public enum casinoGames {
 
         HOME,
+        RESETTING,
         GAME_SELECTOR,
         BLACKJACK
 
